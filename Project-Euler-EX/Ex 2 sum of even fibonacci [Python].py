3de359f35f60b4611fe782d7ebd4a690b70f8acf
Aeolus0 <dhash+git@outlook.com>
2013-12-20 18:21:43 +0530

    Finised Ex2
    Sum of all even fibonacii sumbers who dont exceed 4 million

---
+++ Project-Euler-EX/Ex 2 sum of even fibonacci [Python].py
@@ -0,0 +1,21 @@
+"""Sum of all even fibonacci numbers from 2 to x [400 million]"""
+
+fib = [1, 1, 2]
+
+def next_fib():
+    global fib
+    fib.append(fib[1] + fib[2])
+    del fib[0]
+
+def add_fib(final_number):
+    total_fib = 0
+    global fib
+    for iterator in range(0,final_number + 1):
+        if final_number <= fib[len(fib) - 1]:
+            break
+        if fib[len(fib) - 1] % 2 == 0:
+            total_fib += fib[len(fib) - 1]
+        next_fib()
+    return total_fib
+
+print add_fib(4000000)
