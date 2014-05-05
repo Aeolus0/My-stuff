7d45f073f5b90a26f573f5d09fcd18ca7feaf7bb
dhash <dhash@outlook.com>
2014-05-05 22:53:40 +0530

    Refactored to make clearer

--- Project-Euler-EX/Ex3 Largest Prime factor.py
+++ Project-Euler-EX/Ex3 Largest Prime factor.py
@@ -1,15 +1,17 @@
 __author__ = 'dhash'
 import math
+
+
 def make_primes_factors(lim):
     prime_array = []
-    for iter in range(3, int(math.sqrt(lim)) + 1, 2):
-        if lim % iter == 0:
-            prime_array.append(iter)
+    for is_ele in range(3, int(math.sqrt(lim)) + 1, 2):
+        if lim % is_ele == 0:
+            prime_array.append(is_ele)
     for elem in prime_array:
         for remove_ind in range(0, len(prime_array)):
             if prime_array[remove_ind] > elem:
                 if prime_array[remove_ind] % elem == 0:
-                    del(prime_array[remove_ind])
+                    del (prime_array[remove_ind])
                     break
 
     prime_array.append(1)
@@ -18,5 +20,6 @@ def make_primes_factors(lim):
     prime_array.sort()
     return prime_array
 
-input = int(input("to find the prime factors of a number, enter it: "))
-print(make_primes_factors(input))
+
+input_to_find_prime_factors = int(input("to find the prime factors of a number, enter it: "))
+print(make_primes_factors(input_to_find_prime_factors))
